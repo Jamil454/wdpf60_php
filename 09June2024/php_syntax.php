3d64f8301d48d493f248267fc82e10a65c156741
Hasanat1992 <151622839+Hasanat1992@users.noreply.github.com>
2024-06-09 13:17:55 +0600

    first commit

---
+++ 09June2024/php_syntax.php
@@ -0,0 +1,16 @@
+<?php
+//full syntax
+echo "Hello World<br>";
+?>
+
+<?
+
+//open short tag
+echo "Hello World with short tag";
+?>
+
+<?="Hello World with short circuit";
+
+//open short circuit
+
+?>
