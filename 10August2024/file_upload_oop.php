7aa1c9befdf59bc2621cfc71d37ca225cb6c0fe0
Hasanat1992 <151622839+Hasanat1992@users.noreply.github.com>
2024-08-10 13:12:44 +0600

    10 August 2024

---
+++ 10August2024/file_upload_oop.php
@@ -0,0 +1,30 @@
+
+<?php include_once "file_class.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File upload using oop</title>
+</head>
+<body>
+
+</body>
+    <?php
+
+     if(isset($_POST['submit'])){
+
+       $info =($_FILES['photo']);
+       $obj = new File($info);
+       $obj->upload();
+    }
+    ?>
+
+    <form method="post"  enctype="multipart/form-data">
+    <input type="file" name="photo"> <br>
+    <input type="submit" name="submit" value="UPLOAD">
+
+    </form>
+</body>
+</html>
