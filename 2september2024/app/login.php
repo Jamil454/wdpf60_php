1628dc898c89fc9242f6b0a59095e973c0f45ba5
Hasanat1992 <mdrakib6767@gmail.com>
2024-09-02 13:29:44 +0600

    2 Sep 23024

---
+++ 2september2024/app/login.php
@@ -0,0 +1,24 @@
+<?php
+include_once("dbconfig.php");
+
+    session_start();
+
+if (isset($_POST['login'])){
+    $email = $_POST['email'];
+    $password=$_POST['password'];
+    $password = md5($password);
+
+    $result = $db->query("SELECT * FROM users WHERE email = '$email' AND password = '$password'");
+    $row = $result->fetch_assoc();
+
+    if($result->num_rows==0){
+        $_SESSION['login_err']= "Email or password is wrong. Please try again";
+        header("Location: index.php");
+     }else{
+        $_SESSION['email']= $email;
+        $_SESSION['name']= $row['fullname'];
+        header("Location: dashboard.php");
+     }
+  }
+
+?>
