efc39b1459c8cf243101a2288f8e2e00944c673b
Hasanat1992 <mdrakib6767@gmail.com>
2024-09-11 13:17:55 +0600

    10-Sep-2024

--- 10-Sep-2024/admin/index.php
+++ 10-Sep-2024/admin/index.php
@@ -134,7 +134,7 @@
     <script src="assets/js/jquery.slimscroll.js"></script>
     <script src="assets/js/waves.min.js"></script>
 
-    <script src="../plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
+    <script src="plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
 
     <!-- App js -->
     <script src="assets/js/app.js"></script>
---
+++ 10-Sep-2024/index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <!-- css  -->
+	<?php include "parts/front/css.php" ?>
+</head>
+
+<body class="home-2">
+
+	<!-- START PRELOADER -->
+	<div id="page-preloader">
+		<div class="preloader-wrench"></div>
+	</div>
+	<!-- END PRELOADER --> 
+    
+	<!-- START HEADER SECTION -->
+	<?php include "parts/front/header.php" ?>
+	<!-- END HEADER SECTION -->
+	
+	<!-- START SLIDER SECTION -->
+	<?php include "parts/front/slider.php" ?>
+	<!-- END SLIDER SECTION  -->
+	
+	<!-- START ABOUT SECTION -->
+   
+    <!-- END ABOUT SECTION -->
+	
+	<!-- START SERViCE SECTION -->
+	<?php include "parts/front/service.php" ?>
+    <!-- END SERViCE SECTION -->
+	
+	<!-- START GALLERY SECTION -->
+	<?php include "parts/front/gallery.php" ?>
+    <!-- END GALLERY SECTION -->
+	
+	
+	<!-- START VIDEO & FAQ -->
+    <?php include "parts/front/faq.php" ?>
+    <!-- END VIDEO & FAQ -->
+	
+	<!-- START DOCTOR SECTION -->
+    <?php include "parts/front/doctor.php" ?>
+    <!-- END DOCTOR SECTION -->
+	
+	<!-- START BLOG SERCTION -->
+    <section id="blog" class="section-padding">
+        <div class="container">
+            <div class="row">
+				<div class="col-lg-7 text-center mx-auto">
+                    <div class="section-title">
+                        <h3>Today's<span> News</span></h3>
+                        <span class="line"></span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+				</div>
+				<!-- end section title -->
+			</div>
+			<div class="row">
+				<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-lg-0 mb-md-4 mb-sm-4 mb-4">
+					<div class="single-blog-home">
+						<div class="single-blog-home-img">
+							<a href="#"><img class="img-fluid" src="assets/img/blog/1.jpg" alt=""></a>
+							<div class="single-blog-home-meta">
+								<span class="post-date"><i class="lnr lnr-calendar-full"></i> 15 Dec</span>
+								<span class="post-user"><i class="lnr lnr-user"></i> Admin</span>
+								<span class="post-comment"><i class="lnr lnr-bubble"></i> 5 comments</span>
+							</div>
+						</div>
+						<a href="#"><h5>Digital Bariatric Surgery</h5></a>
+						<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis eges vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+						<a href="#" class="blog-home-rmbtn">Continue <i class="icofont icofont-long-arrow-right"></i></a>	
+					</div>
+				</div>
+				<!-- end single blog -->
+				<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-lg-0 mb-md-4 mb-sm-4 mb-4">
+					<div class="single-blog-home">
+						<div class="single-blog-home-img">
+							<a href="#"><img class="img-fluid" src="assets/img/blog/3.jpg" alt=""></a>
+							<div class="single-blog-home-meta">
+								<span class="post-date"><i class="lnr lnr-calendar-full"></i> 16 Dec</span>
+								<span class="post-user"><i class="lnr lnr-user"></i> Jone</span>
+								<span class="post-comment"><i class="lnr lnr-bubble"></i> 6 comments</span>
+							</div>
+						</div>
+						<a href="#"><h5>Obesity: A Growing Epidemic</h5></a>
+						<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis eges vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+						<a href="#" class="blog-home-rmbtn">Continue <i class="icofont icofont-long-arrow-right"></i></a>	
+					</div>
+				</div>
+				<!-- end single blog -->
+				<div class="col-lg-4 col-md-6 col-sm-12 col-12">
+					<div class="single-blog-home">
+						<div class="single-blog-home-img">
+							<a href="#"><img class="img-fluid" src="assets/img/blog/2.jpg" alt=""></a>
+							<div class="single-blog-home-meta">
+								<span class="post-date"><i class="lnr lnr-calendar-full"></i> 17 Dec</span>
+								<span class="post-user"><i class="lnr lnr-user"></i> Admin</span>
+								<span class="post-comment"><i class="lnr lnr-bubble"></i> 7 comments</span>
+							</div>
+						</div>
+						<a href="#"><h5>After Surgery Bed Rest</h5></a>
+						<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis eges vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+						<a href="#" class="blog-home-rmbtn">Continue <i class="icofont icofont-long-arrow-right"></i></a>	
+					</div>
+				</div>
+				<!-- end single blog -->
+			</div>
+        </div>
+        <!--- END CONTAINER -->
+    </section>
+    <!-- END BLOG SERCTION -->
+	
+	<!-- SINGLE DOCTOR PROMO -->
+	<div class="single-doc-promo bg-theme mt-5">
+        <div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-12 col-12">
+					<div class="single-doc-promo-box-img">
+						<img class="img-fluid" src="assets/img/bg/doc-promo.png" alt="">
+					</div>
+				</div>
+				<div class="col-lg-9 col-md-9 col-sm-12 col-12">
+					<div class="single-doc-promo-box">
+						<div class="row"> 
+							<div class="col-lg-9">
+								<div class="single-doc-promo-content">
+								     <h5>If you need any home service</h5>
+									 <p>Feel free to call us 24hr emergency number</p>
+									 <a href="#"><i class="icofont icofont-phone"></i>+88 315 67 39</a>
+								</div> 
+							</div> 
+							<div class="col-lg-3 mt-4">
+								<a href="#" class="single-doc-promo-btn fadeInUp animated">Contact Us</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- END SINGLE DOCTOR PROMO -->
+	
+    <!-- START FOOTER -->
+    <?php include "parts/front/footer.php" ?>
+    <!-- END FOOTER -->
+	
+	
+    <!-- Latest jQuery -->
+    <?php include "parts/front/js.php" ?>
+</body>
+
+</html>
