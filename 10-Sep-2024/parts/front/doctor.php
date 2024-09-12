50efadacfdb870e36bbcdbe21434a0d707212303
Hasanat1992 <mdrakib6767@gmail.com>
2024-09-12 13:13:39 +0600

    10-Sep-2024

--- 10-Sep-2024/parts/front/doctor.php
+++ 10-Sep-2024/parts/front/doctor.php
@@ -1,104 +1,44 @@
 <section id="doctor" class="section-padding bg-gray">
-        <div class="container">
-            <div class="row">
-				<div class="col-lg-7 text-center mx-auto">
-                    <div class="section-title">
-                        <h3>Our<span> Specialities</span></h3>
-                        <span class="line"></span>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    </div>
-				</div>
-				<!-- end section title -->
-			</div>
-			<div class="row">
-				<div class="team-slider owl-carousel owl-theme">
-					<div class="single-doctor single-doctor-warp">
-						<img class="img-fluid" src="assets/img/team/5.jpg" alt="" />
-						<div class="single-doctor-info">
-							<h4>Revenna Warner</h4>
-							<span>Neurologist</span> 
-						</div>
-						<div class="single-doctor-mask">
-							<div class="single-doctor-mask-inner">
-								<h5>About Doctor</h5>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-								<ul>
-								   <li><a href="#">Get Appointment</a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<!-- end single doctor -->	
-					<div class="single-doctor single-doctor-warp">
-						<img class="img-fluid" src="assets/img/team/4.jpg" alt="" />
-						<div class="single-doctor-info">
-							<h4>Stevest Henry</h4>
-							<span>Ophthalmologist</span> 
-						</div>
-						<div class="single-doctor-mask">
-							<div class="single-doctor-mask-inner">
-								<h5>About Doctor</h5>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-								<ul>
-								   <li><a href="#">Get Appointment</a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<!-- end single doctor -->
-					<div class="single-doctor single-doctor-warp">
-						<img class="img-fluid" src="assets/img/team/3.jpg" alt="" />
-						<div class="single-doctor-info">
-							<h4>Williums Kevins</h4>
-							<span>Dermatologist</span> 
-						</div>
-						<div class="single-doctor-mask">
-							<div class="single-doctor-mask-inner">
-								<h5>About Doctor</h5>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-								<ul>
-								   <li><a href="#">Get Appointment</a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<!-- end single doctor -->
-					<div class="single-doctor single-doctor-warp">
-						<img class="img-fluid" src="assets/img/team/1.jpg" alt="" />
-						<div class="single-doctor-info">
-							<h4>Kewillues Jenifer</h4>
-							<span>Radiologist</span> 
-						</div>
-						<div class="single-doctor-mask">
-							<div class="single-doctor-mask-inner">
-								<h5>About Doctor</h5>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-								<ul>
-								   <li><a href="#">Get Appointment</a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<!-- end single doctor -->
-					<div class="single-doctor single-doctor-warp">
-						<img class="img-fluid" src="assets/img/team/2.jpg" alt="" />
-						<div class="single-doctor-info">
-							<h4>Marquis Williums</h4>
-							<span>Urologist</span> 
-						</div>
-						<div class="single-doctor-mask">
-							<div class="single-doctor-mask-inner">
-								<h5>About Doctor</h5>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-								<ul>
-								   <li><a href="#">Get Appointment</a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<!-- end single doctor -->
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-7 text-center mx-auto">
+				<div class="section-title">
+					<h3>Our<span> Specialities</span></h3>
+					<span class="line"></span>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 				</div>
 			</div>
-        </div>
-        <!--- END CONTAINER -->
-    </section>
+			<!-- end section title -->
+		</div>
+		<div class="row">
+			<div class="team-slider owl-carousel owl-theme">
+
+				<?php
+				include_once "admin/dbconfig.php";
+				$sql = $db->query("SELECT * FROM  doctors");
+				while ($row = $sql->fetch_assoc()) {
+
+				?>
+					<div class="single-doctor single-doctor-warp">
+						<img class="img-fluid" src="admin/doctors/<?php echo $row['photo'] ?>" alt="" />
+						<div class="single-doctor-info">
+							<h4> <td><?php echo $row['doctorName'] ?></td></h4>
+							<span> <td><?php echo $row['specilization'] ?></td></span>
+						</div>
+						<div class="single-doctor-mask">
+							<div class="single-doctor-mask-inner">
+								<h5>About Doctor</h5>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+								<ul>
+									<li><a href="#">Get Appointment</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				<?php  } ?>
+				<!-- end single doctor -->
+			</div>
+		</div>
+	</div>
+	<!--- END CONTAINER -->
+</section>
